2751ada422a113696812f2a30fec49e1a57d43a6
“Parvez <iitd.parvez@gmail.com>
2022-09-05 10:57:15 -0400

    fix messed up docker compose prod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          yoskdjhfbsdkfjb there!
+          yoskdjhfbsdkfjb there! feature changes
         </p>
         <a
           className="App-link"
